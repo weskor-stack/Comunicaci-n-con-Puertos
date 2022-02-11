adaa325c63461c6c2150de4fcb30b49038a178ce
weskor_stack <weskorgenesis@gmail.com>
2022-02-10 20:55:37 -0600

    Adición en guardar información en archivo txt

--- hola.py
+++ hola.py
@@ -1,5 +1,10 @@
-import socket
-print(socket.gethostbyname(socket.gethostname()))
+mensaje1 = 'hola mundo'
+mensaje2 = "hola mundo"
+mensaje3 = """Hola
+hola
+hola mundo"""
 
-while True:
-    import socket_echo_server
+file = open('ejemplo.txt','w')
+file.write("mensaje 1: " + mensaje1+"\n")
+file.write("mensaje 2: " + mensaje2+"\n")
+file.write("mensaje 3: " + mensaje3)
